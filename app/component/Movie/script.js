86e1e8e44f04887cda846bc61988bcaf15e7b0e5
NathClrg <nathan.clerge@gmail.com>
2026-04-22 16:58:49 +0200

    pas finis

---
+++ app/component/Movie/script.js
@@ -0,0 +1,13 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+let dataFile = await fetch(data); // data is the file path
+let dataHtml = await dataFile.text();
+let Movie = {};
+
+Movie.format = function (data) {
+  let html = template;
+  html = html.replace("{{li}}", dataHtml);
+  return html;
+};
+
+export { Movie };
